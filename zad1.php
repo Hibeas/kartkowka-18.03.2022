5e77335d92a19d5598a0ae698679f6029b52c3c4
Hibeas <79516378+Hibeas@users.noreply.github.com>
2022-03-18 10:54:57 +0100

    imie i nazwiskop najs

--- zad1.php
+++ zad1.php
@@ -13,6 +13,10 @@ $Nr=$_POST['Nr'];
 
 $Wytnij = substr($Imie, 0, $Nr-1);
 $Wytnij2 = substr($Imie, $Nr, null);
-echo $Wytnij.$Wytnij2;
 
+$Wytnij3 = substr($Nazwisko, 0, $Nr-1);
+$Wytnij4 = substr($Nazwisko, $Nr, null);
+
+echo $Wytnij.$Wytnij2."<br>";
+echo $Wytnij3.$Wytnij4."<br>";
 ?>
